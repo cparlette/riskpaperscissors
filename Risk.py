fecf7c9c6e771189146e19fcadc8e542379de4c0
cparlette <cparlette@gmail.com>
2018-11-03 12:49:52 -0400

    Add North America locations

--- Risk.py
+++ Risk.py
@@ -12,6 +12,14 @@ class Risk():
 		)
 		self.locations = []
 		self.locations.append(Location(self,80,112,"Alaska"))
+		self.locations.append(Location(self,168,112,"NW Territory"))
+		self.locations.append(Location(self,158,161,"Alberta"))
+		self.locations.append(Location(self,222,160,"Ontario"))
+		self.locations.append(Location(self,294,165,"Quebec"))
+		self.locations.append(Location(self,345,71,"Greenland"))
+		self.locations.append(Location(self,161,233,"Western US"))
+		self.locations.append(Location(self,237,247,"Eastern US"))
+		self.locations.append(Location(self,170,321,"Central America"))
 
 	def process_keydown(self, key):
 		pass
