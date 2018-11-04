410899e45cb8cf69784c57ac73cc07091814c325
cparlette <cparlette@gmail.com>
2018-11-03 23:07:30 -0400

    Add neighbors list to each location

--- Location.py
+++ Location.py
@@ -2,13 +2,14 @@ from constants import *
 import pygame
 
 class Location():
-	def __init__(self, risk_game, location_id, center_x, center_y, name):
+	def __init__(self, risk_game, location_id, center_x, center_y, name, neighbors):
 		self.location_id = location_id
 		self.armies = "0"
 		self.bg_color = WHITE
 		self.center_x = center_x
 		self.center_y = center_y
 		self.name = name
+		self.neighbors = neighbors
 		self.width = 30
 		self.height = 30
 		self.risk_game = risk_game
