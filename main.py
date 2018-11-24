26a40a31406a708b60522054e4c9dc68aaba7605
cparlette <cparlette@gmail.com>
2018-11-24 11:27:18 -0500

    Moved around main menu

--- main.py
+++ main.py
@@ -16,21 +16,19 @@ def main():
 
 	### Menu Creation ###
 	main_menu = Menu(screen, "Risk Paper Scissors!", WHITE, "tahoma", BLACK)
-	main_menu.add_button("Risk", 200, 100, WHITE, str("Risk"), BLACK, WIDTH / 2, (HEIGHT * 2 / 5))
-	#main_menu.add_button("RPS", 200, 100, WHITE, str("RPS"), BLACK, WIDTH / 2, (3 * HEIGHT / 5))
-	main_menu.add_button("Settings", 200, 100, WHITE, str("Settings"), BLACK, WIDTH / 4, (HEIGHT * 3 / 5))
-	main_menu.add_button("Help", 200, 100, WHITE, str("Help"), BLACK, (WIDTH * 3 / 4), (HEIGHT * 3 / 5))
-	main_menu.add_button("Quit", 200, 100, WHITE, str("Quit"), BLACK, WIDTH / 2, (HEIGHT * 4 / 5))
+	main_menu.add_button("Risk", 200, 100, WHITE, str("Risk"), BLACK, WIDTH / 2, (HEIGHT * 2 / 6))
+	main_menu.add_button("Settings", 200, 100, WHITE, str("Settings"), BLACK, WIDTH / 2, (HEIGHT * 3 / 6))
+	main_menu.add_button("Help", 200, 100, WHITE, str("Help"), BLACK, WIDTH / 2, (HEIGHT * 4 / 6))
+	main_menu.add_button("Quit", 200, 100, WHITE, str("Quit"), BLACK, WIDTH / 2, (HEIGHT * 5 / 6))
  
 	settings_menu = Menu(screen, "Settings", WHITE, "tahoma", BLACK)
-	settings_menu.add_button("Main Menu", 300, 100, WHITE, str("Main Menu"), BLACK, WIDTH / 2, HEIGHT / 2)
+	settings_menu.add_button("Main Menu", 200, 100, WHITE, str("Main Menu"), BLACK, WIDTH / 2, HEIGHT / 2)
 
 	help_menu = Menu(screen, "Help", WHITE, "tahoma", BLACK)
-	help_menu.add_button("Main Menu", 300, 100, WHITE, str("Main Menu"), BLACK, WIDTH / 2, HEIGHT / 2)
+	help_menu.add_button("Main Menu", 200, 100, WHITE, str("Main Menu"), BLACK, WIDTH / 2, HEIGHT / 2)
 
 	### Game Init ###
 	risk = Risk(screen)
-	#rps = RPS(screen, 5, 5)
 	game = None
 
 	# Assign starting menu (set to None for no starting menu)
